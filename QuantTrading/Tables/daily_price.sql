aee52dd785018b7bcaffdcd0ca5b8669c0644cd8
RayPeiqingHe <ray.peiqing.he@gmail.com>
2018-01-21 17:12:10 -0500

    Changes to use MySql

--- QuantTrading/Tables/daily_price.sql
+++ QuantTrading/Tables/daily_price.sql
@@ -3,8 +3,8 @@ id int NOT NULL AUTO_INCREMENT,
 data_vendor_id int NOT NULL,
 symbol_id int NOT NULL,
 price_date datetime NOT NULL,
-created_date datetime NOT NULL,
-last_updated_date datetime NOT NULL,
+created_date datetime NOT NULL DEFAULT NOW(),
+last_updated_date datetime NOT NULL DEFAULT NOW(),
 open_price decimal(19,4) NULL,
 high_price decimal(19,4) NULL,
 low_price decimal(19,4) NULL,
@@ -15,3 +15,6 @@ constraint pk_daily_price PRIMARY KEY (id),
 constraint fk_data_vendor_id foreign KEY (data_vendor_id) references data_vendor(id),
 constraint fk_symbol_id foreign KEY (symbol_id) references symbol(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE daily_price  ADD UNIQUE INDEX ix_dail_price (symbol_id, price_date);
--- QuantTrading/Views/vw_last_missing_price_date.sql
+++ QuantTrading/Views/vw_last_missing_price_date.sql
@@ -1,29 +1,14 @@
-IF EXISTS(SELECT * FROM SYS.views WHERE NAME = 'vw_last_missing_price_date' AND SCHEMA_NAME(SCHEMA_ID) = 'DBO')
-BEGIN
-
-    DROP VIEW DBO.vw_last_missing_price_date
-
-	PRINT 'DROP VIEW DBO.vw_last_missing_price_date'
-
-END
-
-GO
-
-CREATE VIEW DBO.vw_last_missing_price_date
+CREATE VIEW vw_last_missing_price_date
 AS
-SELECT s.id, ticker
-, COALESCE(CONVERT(char(10),DATEADD(day, 1, MAX(price_date)), 126), '2000-01-01') AS last_price_date 
-FROM symbol s
-LEFT JOIN (SELECT max(corporate_action_date) as d_date, symbol_id from corporate_action GROUP BY symbol_id) d 
-ON s.id = d.symbol_id
-LEFT JOIN daily_price p
-ON p.symbol_id = s.id AND (d.d_date IS NULL OR d.d_date <= p.price_date)
-GROUP BY s.id, ticker
-HAVING Convert(date, getdate()) >= COALESCE(CONVERT(char(10),DATEADD(day, 1, MAX(price_date)), 126), '2000-01-01')
-
-GO
-
-
-PRINT 'CREATE VIEW DBO.vw_last_missing_price_date'
-
-
+select 
+s.id
+,s.ticker
+,coalesce((max(p.price_date) + interval 1 day),'2000-01-01') AS `last_price_date` 
+from ((symbol s 
+	   left join (select max(corporate_action.corporate_action_date) 
+				  AS `d_date`, corporate_action.symbol_id AS `symbol_id`
+				  from corporate_action group by corporate_action.symbol_id) d
+	   on((s.id = d.symbol_id))) left join daily_price p on(((p.symbol_id = s.id) 
+	   and (isnull(d.d_date) or (d.d_date <= p.price_date))))) 
+	   group by s.id, s.ticker having (cast(now() as date) >= coalesce((max(p.price_date) + interval 1 day),'2000-01-01'))
+           and (cast(now() as date) >= coalesce((max(p.last_updated_date)),'2000-01-01'));;
