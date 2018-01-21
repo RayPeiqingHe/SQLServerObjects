24e64077f5dc6afc48fdac06bfa80ff98aa627b4
RayPeiqingHe <ray.peiqing.he@gmail.com>
2018-01-21 17:55:02 -0500

    Add default value for date column in sumbol table

--- QuantTrading/Tables/symbol.sql
+++ QuantTrading/Tables/symbol.sql
@@ -6,8 +6,8 @@ instrument varchar(64) NOT NULL,
 name varchar(255) NULL,
 sector varchar(255) NULL,
 currency varchar(32) NULL,
-created_date datetime NOT NULL,
-last_updated_date datetime NOT NULL,
+created_date datetime NOT NULL DEFAULT NOW(),
+last_updated_date datetime NOT NULL DEFAULT NOW(),
 CONSTRAINT pk_symbol PRIMARY KEY (id),
 CONSTRAINT fk_exchange FOREIGN KEY (exchange_id) references exchange(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
